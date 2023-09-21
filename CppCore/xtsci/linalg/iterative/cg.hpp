49ce12ca55d65a6a2b18c1f45096dd3c2c7c5caf
Rohit Goswami <rog32@hi.is>
2023-09-21 17:26:26 +0000

    ENH: Initialize a simple conjugate_gradient

---
+++ CppCore/xtsci/linalg/iterative/cg.hpp
@@ -0,0 +1,86 @@
+#pragma once
+// MIT License
+// Copyright 2023--present Rohit Goswami <HaoZeke>
+#include "xtensor/xexpression.hpp"
+#include "xtensor/xmath.hpp"
+#include "xtensor/xtensor.hpp"
+#include "xtensor/xview.hpp"
+
+#include "xtensor-blas/xlinalg.hpp"
+namespace xts {
+namespace linalg {
+namespace iterative {
+template <typename E1, typename E2, typename E3, typename Preconditioner>
+// Straight port of the Eigen implementation
+void conjugate_gradient(const xt::xexpression<E1>& mat_expr,
+                        const xt::xexpression<E2>& rhs_expr,
+                        xt::xexpression<E3>& x_expr,
+                        const Preconditioner& precond,
+                        int& iterations,
+                        typename E3::value_type& final_error)
+{
+    const auto& mat = mat_expr.derived_cast();
+    const auto& rhs = rhs_expr.derived_cast();
+    auto& x = x_expr.derived_cast();
+
+    using RealScalar = typename E3::value_type;
+
+    RealScalar tol = final_error;
+    int maxIters = iterations;
+
+    xt::xarray<RealScalar> residual = rhs - xt::linalg::dot(mat, x);
+
+    RealScalar rhsNorm2 = xt::linalg::norm(rhs, 2);
+    rhsNorm2 *= rhsNorm2;
+
+    if (rhsNorm2 == 0) {
+        x.fill(0);
+        iterations = 0;
+        final_error = 0;
+        return;
+    }
+
+    RealScalar threshold = std::max(tol * tol * rhsNorm2, std::numeric_limits<RealScalar>::min());
+
+    RealScalar residualNorm2 = xt::linalg::norm(residual, 2);
+    residualNorm2 *= residualNorm2;
+
+    if (residualNorm2 < threshold) {
+        iterations = 0;
+        final_error = std::sqrt(residualNorm2 / rhsNorm2);
+        return;
+    }
+
+    auto searchDirection = precond.solve(residual);
+
+    RealScalar dotProductNew = xt::linalg::dot(residual, searchDirection)();
+    int i = 0;
+
+    while (i < maxIters) {
+        auto tmp = xt::linalg::dot(mat, searchDirection);
+        RealScalar alpha = dotProductNew / xt::linalg::dot(searchDirection, tmp)();
+
+        x += alpha * searchDirection;
+        residual -= alpha * tmp;
+
+        residualNorm2 = xt::linalg::norm(residual, 2);
+        residualNorm2 *= residualNorm2;
+
+        if (residualNorm2 < threshold)
+            break;
+
+        auto z = precond.solve(residual);
+        RealScalar dotProductOld = dotProductNew;
+        dotProductNew = xt::linalg::dot(residual, z)();
+        RealScalar beta = dotProductNew / dotProductOld;
+        searchDirection = z + beta * searchDirection;
+
+        i++;
+    }
+
+    final_error = std::sqrt(residualNorm2 / rhsNorm2);
+    iterations = i;
+}
+} // namespace iterative
+} // namespace linalg
+} // namespace xts
